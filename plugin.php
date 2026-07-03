ae77d67f3828d1ccdde7cab76562642a9afadc02
TylerCorrad <coralundethewater@gmail.com>
2026-07-03 16:50:43 -0500

    first commit

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'id' => 'mobile-responsive-plugin',
+    'version' => '1.0.0',
+    'name' => 'mobile responsive plugin',
+    'author' => 'Tyler Corradine',
+    'description' => 'adecua UI de osTicket para que sea más amigable con dispositivos móviles',
+    'plugin' => 'class.MobileResponsivePlugin.php:MobileResponsivePlugin'
+];
